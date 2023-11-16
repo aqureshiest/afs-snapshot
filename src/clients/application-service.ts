a63d96ad6e4ab56897f5e032225afbedc762af5d
Chris Ciervo <christopherciervo@gmail.com>
2023-11-16 11:54:39 -0800

    LA-106_prototyping-application-service-client

---
+++ src/clients/application-service.ts
@@ -0,0 +1,146 @@
+import  type { PluginContext } from "@earnest-labs/microservice-chassis/PluginContext.js";
+import SensitiveString from "@earnest-labs/ts-sensitivestring";
+import type { Jwt, JwtPayload, SignOptions, VerifyOptions } from "jsonwebtoken";
+import axios from 'axios';
+
+export default class ApplicationServiceClient {
+  private context: PluginContext;
+  private accessKey: string;
+  private token: string;
+
+  constructor(context: PluginContext) {
+    const key = SensitiveString.ExtractValue(context.env.ACCESS_KEY) || "";
+  
+    this.accessKey = Buffer.from(key).toString("base64");
+    this.context = context;
+  }
+
+  /**
+   * Builds authorization headers for a given request
+   */
+  buildAuthorizationHeaders() {
+    return {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${this.accessKey}`,
+    };
+  }
+
+  /**
+   * Requests a new JWT if one doesnt exist or 
+   * the current token is expired
+   */
+  async getToken(): Promise<string> {
+    const { token } = this;
+
+    if (token) {
+      const split = token.split(".");
+
+      const payload = JSON.parse(
+        Buffer.from(split[1], "base64").toString("utf8")
+      );
+
+      const { exp } = payload;
+
+      if (Date.now() - exp > exp) this.token = await this.requestToken();
+
+    } else { 
+      this.token = await this.requestToken();
+    }
+    
+    return this.token;
+  }
+
+/**
+ * Requests a new JWT from application-service
+ */ 
+  async requestToken(): Promise<string> {
+    const { context: { logger } } = this;
+
+    try {
+      const { data } = await axios({
+        method: "post",
+        url: 'http://host.docker.internal:4500/auth',
+        data: {
+          sub: "apply-flow-service",
+          email: "test@earnest.com",
+        },
+        headers: this.buildAuthorizationHeaders(),
+      });
+     
+      return data.token;
+    } catch (error) {
+      logger.error({
+        message: "Failed to request JWT from application-service",
+        error: error.message,
+      });
+
+      throw error;
+    }
+  }
+
+  /**
+   * Performs a graphql Query 
+   */
+  async query() {
+    const { context: { logger }} = this;
+    /**
+     * TODO: come up with approach for dynamically generating graphql query strings
+     */
+    const placeholder = "";
+    try {
+      const response = await axios({
+        method: "get",
+        url: 'http://host.docker.internal:4500/graphql',
+        data: {
+          query: placeholder,
+          meta: {
+            service: "apply-flow-service"
+          },
+
+        },
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: await this.getToken(),
+        }
+      });
+
+      return response.data;
+    } catch (error) {
+      logger.error({
+        message: "Failed to query application-service",
+        error: error.message
+      });
+
+      throw error;
+    }
+  }
+
+  async mutate() {
+   const { context: { logger }} = this;
+   /**
+    * TDOO: come up with approach for dynamically generating graphql mutation strings
+    */
+   try {
+     const response = await axios({
+       method: "post",
+       url: 'http://host.docker.internal:4500/graphql',
+       data: {
+         query: "apply-flow-service",
+         meta: {
+           service: "service"
+         },
+
+       },
+       headers: this.buildAuthorizationHeaders(), 
+     });
+     return response.data;
+   } catch (error) {
+    logger.error({
+      message: "Failed to apply mutation to application-service",
+      error: error.message,
+    });
+
+    throw error;
+   }
+  }
+}
---
+++ src/clients/chassis-plugin.ts
@@ -0,0 +1,21 @@
+import Plugin from "@earnest-labs/microservice-chassis/Plugin.js";
+import PluginContext from "@earnest-labs/microservice-chassis/PluginContext.js";
+import ApplicationServiceClient from "./application-service.js";
+
+export const plugin: Plugin = {
+  name: "clients",
+  version: "1.0.0",
+  registerOrder: 0,
+  register: async (context: PluginContext ) => {
+    const client = new ApplicationServiceClient(context);
+    const test = await client.requestToken();
+
+    /**
+     * TODO:
+     * Check if client has a token
+     * and if that token has expired. Token expires after 30 minutes
+     *
+     */
+    // set the graphlql client on the context; 
+  },
+};
