a098cf5704ba097b96cf0948d67b162bbd652936
Chris Ciervo <christopherciervo@gmail.com>
2023-12-06 15:39:45 -0800

    LA-106_removing-comments-from-getInputs

--- src/api/handlers/get-inputs.ts
+++ src/api/handlers/get-inputs.ts
@@ -5,36 +5,6 @@
  */
 const getInputs: Handler = async function (context, req, res, next) {
 
-  // COMMENTING THIS OUT FOR NOW AS ITS BREAKING UNIT TESTS
-
-  // const ApplicationServiceClient =
-  //   context.loadedPlugins?.applicationServiceClient?.instance;
-
-  // if (!ApplicationServiceClient)
-  //   throw new Error("[1d348234] Unable to load Application Service Client");
-
-  // const fieldKeys = [
-  //   "id",
-  //   "createdAt",
-  //   "deletedAt",
-  //   "name.first",
-  //   "events.event",
-  //   "events.id",
-  //   "events.data",
-  // ];
-
-  // const queryResponse = await ApplicationServiceClient["query"](
-  //   req.params.uuid,
-  //   fieldKeys,
-  // );
-
-  // res.locals.inputs = { request: req, application: queryResponse };
-
-  // TODO: use the manifest (sourced from a previous middleware) to get a list of application
-  // inputs that appear in the manifest
-
-  // const references = res.locals.manifest?.references || [];
-
   const inputs = req.body ? req.body : {};
 
   res.locals.inputs = {
