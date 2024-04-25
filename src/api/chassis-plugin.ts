87f15f18183fcade2752000ed76178aa46c58380
Chris Ciervo <christopherciervo@gmail.com>
2024-04-25 16:40:04 -0700

    fix-merge-conflict

--- src/api/chassis-plugin.ts
+++ src/api/chassis-plugin.ts
@@ -15,9 +15,8 @@ export const plugin: Plugin = {
 
     context.application.use(
       `/apply/*/:id(${constants.UUID_REGEX.source})`,
-      handlers.getManifest.bind(null, context),
-      wrapAsyncHandler(context, authMiddleware),
       wrapAsyncHandler(context, handlers.getManifest),
+      wrapAsyncHandler(context, authMiddleware),
       wrapAsyncHandler(context, handlers.getInputs),
       wrapAsyncHandler(context, handlers.execute),
     );
@@ -67,9 +66,8 @@ export const plugin: Plugin = {
      */
     context.application.use(
       "/apply/*",
-      handlers.getManifest.bind(null, context),
-      wrapAsyncHandler(context, authMiddleware),
       wrapAsyncHandler(context, handlers.getManifest),
+      wrapAsyncHandler(context, authMiddleware),
       wrapAsyncHandler(context, handlers.getInputs),
       wrapAsyncHandler(context, handlers.execute),
     );
