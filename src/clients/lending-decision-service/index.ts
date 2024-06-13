849f905c1baa1b21d2e2182fa94a9eae0505b906
Chris Ciervo <christopherciervo@gmail.com>
2024-06-13 13:26:53 -0700

    extra caution

--- src/clients/lending-decision-service/index.ts
+++ src/clients/lending-decision-service/index.ts
@@ -758,7 +758,7 @@ export default class LendingDecisionServiceClient extends Client {
           : "default",
         rateAdjustmentData: {
           name: application?.partnerName ? application.partnerName : "juno",
-          amount: application?.partnerDiscountAmount
+          amount: (application?.partnerDiscountAmount && !Number.isNaN(Number(application?.partnerDiscountAmount)))
             ? Number(application.partnerDiscountAmount)
             : 0,
         },
