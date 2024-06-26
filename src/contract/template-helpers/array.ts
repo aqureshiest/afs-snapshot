4d4a2df49050c327021ed7b1fe007f26f637a778
AJ Ignacio <64221292+alejandro-ignacio@users.noreply.github.com>
2024-06-25 19:12:43 -0700

    LA-852 [prod] Failed to get school error; Error message shown under Academic Info section on review screen (#301)

--- src/contract/template-helpers/array.ts
+++ src/contract/template-helpers/array.ts
@@ -1,6 +1,23 @@
-const findInArray = function (arr, property, value, return_property) {
-  if (Array.isArray(arr) && property && return_property) {
-    const item = arr.find((item) => item[property] === value);
+const findInArray = function (...args) {
+  const [arr, property, value, return_property] = args;
+  const context = args[args.length - 1].data.context;
+  let parsedArray;
+  try {
+    /**
+     * arr is being passed in as a string representation of the array
+     * via (contract 'getDegrees') in the handlebars definition
+     * We need to convert it back to an actual array
+     */
+    parsedArray = JSON.parse(arr);
+  } catch (ex) {
+    context.logger.error({
+      message: `[0c9c170f] ${ex.message}`,
+      error: ex,
+    });
+  }
+
+  if (Array.isArray(parsedArray) && property && return_property) {
+    const item = parsedArray.find((item) => item[property] === value);
     return item ? item[return_property] : null;
   }
 };
