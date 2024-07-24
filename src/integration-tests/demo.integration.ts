6fa03ef00cce244f7cc688af2b8f898dc21607c3
Early Ehlinger <earlye@gmail.com>
2024-07-24 11:34:43 -0500

    INF-10526/integrate-github-actions

--- src/integration-tests/demo.integration.ts
+++ src/integration-tests/demo.integration.ts
@@ -1,6 +1,6 @@
 import { describe, it } from "node:test";
 import assert from "node:assert";
-import type { Client } from "@earnest/http";
+import { Client } from "@earnest/http";
 
 describe("test ping", () => {
   it("[get /ping", async () => {
