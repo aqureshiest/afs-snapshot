81fe681750113fe5b4a8f1aef59ba53c590c2df0
Rajdeep Debnath <152326376+rajdeep-adroit@users.noreply.github.com>
2024-09-24 00:28:14 +0530

    LA-1293 retired income fix (#521)
    [skip bearer]
    [skip prettier]

--- src/contract/template-helpers/index.ts
+++ src/contract/template-helpers/index.ts
@@ -348,6 +348,9 @@ export function getAdditionalIncomeSourceArray(v1, additionalIncome) {
 export function getAdditionalIncomeSourceTypes(v1) {
   return v1 ? v1.filter((i) => i.type).map((i) => i.type) : undefined;
 }
+export function checkAdditionalIncomeSourceExists(v1) {
+  return v1 ? v1.slice(1).filter((i) => i.type).map((i) => i.type) : undefined;
+}
 
 export function getAdditionalIncomeSourceTypesAsString(v1) {
   const mapping_arr = {
